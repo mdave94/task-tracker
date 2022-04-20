f07f3ae4a6515b4d5775901d857bb4dc2cd1f264
mdave94 <molnardavid2002@gmail.com>
2022-04-20 16:37:31 +0200

    Task.js added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import {useState} from "react"
+
 
 function App() {
+
+  const [tasks,setTasks] = useState([
+    {id:1,
+        text:'Doctors Appointment',
+        day:'Feb 5th at 2:30',
+        reminder: true
+   
+       },
+       {
+           id:2,
+           text:'Meeting',
+           day: 'Feb 6th at 1:31pm '
+       }
+    ])
+
+
+
+  //Delete Task
+  const deleteTask = (id)=>{
+    console.log('DELETE TASK',id);
+  }
+
+
 
 
   return (
    <div className='container'>
       <Header />
-     
+      <Tasks tasks={tasks} onDelete = {deleteTask}/>
     </div>
   );
 }
