6a12657acf125f03e69b3f94c4c8443648c1a1d2
mdave94 <molnardavid2002@gmail.com>
2022-05-30 14:43:56 +0200

    Delete on servere side

--- src/App.js
+++ src/App.js
@@ -1,16 +1,42 @@
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
-import {useState} from "react";
+import {useState,useEffect} from "react";
 import AddTask from "./components/AddTask";
+import { wait } from "@testing-library/user-event/dist/utils";
 
 
 function App() {
   const [showAddTask,setShowAddTask] = useState(false)
-  const [tasks,setTasks] = useState()
+  const [tasks,setTasks] = useState([])
 
+  useEffect(()=> {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks()
+      setTasks(tasksFromServer)
 
+    }
+
+  
+    getTasks()
+  }, [] )
+
+  const fetchTasks = async () => {
+    // important at prod
+    const res = await fetch('http://localhost:5000/tasks')
+    
+    const data = await res.json()
+
+    return data
+  }
+  
   //Delete Task
-  const deleteTask = (id)=>{
+  const deleteTask = async (id)=>{
+    const urlToDelete = "http://localhost:5000/tasks/"+id
+    await fetch(urlToDelete,{
+      method:'DELETE'
+    })
+
+
     setTasks(tasks.filter((task)=>task.id!== id))
   }
 
@@ -24,7 +50,23 @@ function App() {
 
 
 //Add Task
-const addTask = (task) =>{
+const addTask = async (task) =>{
+  const urlToAdd = "http://localhost:5000/tasks/"
+  
+  const res = await fetch(urlToAdd,
+    {
+      method: 'POST',
+      headers: {
+        'Content-type':'application/json'
+      },
+      body:JSON.stringify(task)
+    })
+
+    const data = res.json()
+    
+    // add the current tasks list and add the new one
+    setTasks([...tasks,data])
+
   console.log(task);
 }
 
